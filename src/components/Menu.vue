774ba05b215cb44bc8bc5f1be434f9288c823ba8
Kristian Mandrup <kmandrup@gmail.com>
2017-05-30 13:05:28 +0200

    cleanup menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,13 +2,11 @@
   <md-menu>
     <md-button md-menu-trigger>Manage</md-button>
     <md-menu-content>
-      <md-menu-item @selected="$router.push({ name: 'organisation'})">Organisation</md-menu-item>
-      <md-menu-item @selected="$router.push({ name: 'user'})">User</md-menu-item>
-      <md-menu-item @selected="$router.push({ name: 'repo'})">Repo</md-menu-item>
-      <md-menu-item @selected="$router.push({ name: 'repos'})">Repos</md-menu-item>
-      <md-menu-item @selected="$router.push({ name: 'project'})">Project</md-menu-item>
+      <md-menu-item @selected="$router.push({ name: 'organisations'})">Organisations</md-menu-item>
+      <md-menu-item @selected="$router.push({ name: 'users'})">Users</md-menu-item>
       <md-menu-item @selected="$router.push({ name: 'projects'})">Projects</md-menu-item>
-      <md-menu-item @selected="$router.push({ name: 'environment'})">Environment</md-menu-item>
+      <md-menu-item @selected="$router.push({ name: 'environments'})">Environments</md-menu-item>
+      <md-menu-item @selected="$router.push({ name: 'repos'})">Repos</md-menu-item>
     </md-menu-content>
   </md-menu>
 </template>
